856643d92c03fe66b521bb96d49b916478c767ea
Yuval Simon <63560633+Reven8e@users.noreply.github.com>
2020-10-14 12:59:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from dateutil.relativedelta import relativedelta
 
 import math
 
+import sys
+
+
 # Finding stock and prices
 yf.pdr_override()
 
@@ -13,7 +16,7 @@ stock = input("Enter a stock ticker symbol: ")
 
 now = datetime.datetime.now()
 
-startdate = datetime.datetime.today() + relativedelta(months=-12)
+startdate = datetime.datetime.today() + relativedelta(months=-6)
 
 df = pdr.get_data_yahoo(stock, startdate, now)
 
@@ -26,26 +29,31 @@ df[smaString] = df.iloc[:, 4].rolling(window=ma).mean()
 df = df.iloc[ma:]
 
 # Checking stock prices average
-prices = [math.trunc(df["Close"][i]) for i in df.index]
+try:
+    prices = [math.trunc(df["Close"][i]) for i in df.index]
+    total = sum(prices)
 
-total = sum(prices)
+    amount = len([Int for Int in prices if isinstance(Int, int)])
 
-amount = len([Int for Int in prices if isinstance(Int, int)])
+    average = total / amount
 
-average = total / amount
+    print(f'The average is: {math.trunc(average)}')
 
-print(f'The average is: {math.trunc(average)}')
+except ZeroDivisionError:
+    print('Stock not found!')
+    sys.exit(0)
 
 
 # Checking if you should buy the stock.
-today = pdr.get_data_yahoo(stock, now, now)
+today_stock = pdr.get_data_yahoo(stock, now, now)
 
-numb = [math.trunc(today["High"][i]) for i in today.index]
+numb = [math.trunc(today_stock["High"][i]) for i in today_stock.index]
 
 sum_numb = sum(numb)
+print(f"Current {stock}'s price is: {sum_numb}")
 
 if sum_numb < average:
-    print('I suggest you to buy this stock!')
+    print('I suggest you to buy this stock!\n')
 
 elif sum_numb > average:
     print('I suggest you to NOT buy this stock.')
